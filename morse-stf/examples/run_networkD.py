e313b3816cefd01dcb9c36cd30748ae1dc62a29d
Qizhi Zhang <qizhi.zqz@antgroup.com>
2021-12-21 10:09:27 +0800

    up

--- morse-stf/examples/run_networkD.py
+++ morse-stf/examples/run_networkD.py
@@ -119,58 +119,11 @@ def stf_cnn_test(train_x, train_y, test_x, test_y,keras_weight=None):
     model.save_model(save_file_path="../output/complex_CNN.npz", sess=sess, model_file_machine='R')
 
 
-# def stf_cnnlocal_test(train_x, train_y, test_x, test_y,keras_weight=None):
-#     """
-#     NETWORK C using STF
-#     :param train_x: figure for training
-#     :param train_y: figure for label
-#     :param test_x: figure for training
-#     :param test_y: figure for label
-#     :param keras_weight:   initial weight of format of Keras
-#     :return:
-#     """
-#     sess = tf.compat.v1.Session(StfConfig.target)
-#     epochs = 10
-#     batch_size = 128
-#     learning_rate = 0.01
-#     record_num = train_x.shape[0]
-#     batch_num_per_epoch = record_num // batch_size
-#     train_batch_num = epochs * batch_num_per_epoch
-#     # train_batch_num = 21
-#     print("train_batch_num: " + str(train_batch_num))
-#     pred_batch_num = test_x.shape[0] // batch_size
-#     predict_file_name = "../output/complex_mnist_predict.txt"
-#     # convert dataset
-#     x_train, y_train, x_test, y_test = convert_datasets(train_x=train_x, train_y=train_y,
-#                                                         test_x=test_x, test_y=test_y,
-#                                                         epoch=epochs, batch_size=batch_size)
-#     # build model
-#     model = NetworkD(feature=x_train, label=y_train)
-#     if keras_weight:
-#         # load weights
-#         print("start replace")
-#         model.replace_weight(keras_weight)
-#     # compile model
-#     model.compile()
-#     print("success compile")
-#     print("start train model")
-#     start_time = time.time()
-#     model.train_sgd(learning_rate=learning_rate, batch_num=train_batch_num, l2_regularization=0.0, sess=sess)
-#     end_time = time.time()
-#     print("train time=", end_time - start_time)
-#     # random.random_init(sess)
-#     print("start predict")
-#     model.predict_to_file(sess, x_test, StfConfig.predict_to_file, pred_batch_num=pred_batch_num,
-#                           model_file_machine='R', with_sigmoid=False)
-#     model.save_model(save_file_path="../output/complex_CNN.npz", sess=sess, model_file_machine='R')
-#
 
 if __name__ == "__main__":
-    # budild_mnist("../dataset/")
-    # LeNet_network()
-    # exit()
-    StfConfig.default_fixed_point = 16
-    train_x, train_y, test_x, test_y = load_data(normal=True, small=False)
+
+    #StfConfig.default_fixed_point = 16
+    train_x, train_y, test_x, test_y = load_data(normal=True, small=True)
 
     cnn_baseline(train_x, train_y, test_x, test_y, train=True)
     # exit()
